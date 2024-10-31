400742eb912c9a1d7baca3a24e698270ec12d592
Michael-Aladejobi <mikkystechs@gmail.com>
2024-10-31 23:04:08 +0100

    close scanner

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,15 @@ import java.util.*;
 public class Main{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("What is your name? ");
-        String name = scanner.nextLine();
+        System.out.println("Welcome to X_calc. ");
 
-        System.out.printf("Welcome, " + name + ".");
+        System.out.print("Enter first number: ");
+        int x = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int y = scanner.nextInt();
+
+        scanner.close();
+        System.out.printf("Addition: %d", x+y);
     }
 }
