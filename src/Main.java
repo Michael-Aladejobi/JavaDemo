faa597a52450165da3e83aa53e7c86714837d6fe
Michael-Aladejobi <mikkystechs@gmail.com>
2024-11-01 00:14:36 +0100

    The mortgage Calculator

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
 import java.util.*;
 
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to X_calc. ");
 
-        System.out.print("Enter first number: ");
-        int x = scanner.nextInt();
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
 
-        System.out.print("Enter second number: ");
-        int y = scanner.nextInt();
+        System.out.print("Annual Interest Rate: ");
+        float annualInterestRate = scanner.nextFloat();
+        float montlyInterestRate = (annualInterestRate / 100) / 12;
 
-        scanner.close();
-        System.out.printf("Addition: %d", x+y);
+        System.out.print("Period (Years): ");
+        byte year = scanner.nextByte();
+        int numberOfPayment = year * 12;
+
+        String mortgage = Double.toString(principal * (montlyInterestRate * (Math.pow((1 + montlyInterestRate), numberOfPayment))
+                / (Math.pow((1 + montlyInterestRate), numberOfPayment)-1)));
+
+        System.out.printf("Mortgage: $%s", mortgage);
     }
 }
