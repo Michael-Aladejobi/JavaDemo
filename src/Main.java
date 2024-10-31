f2f530a0a2e8d3c627f90394db124273b06afb9d
Michael-Aladejobi <mikkystechs@gmail.com>
2024-10-31 08:06:04 +0100

    Disp Ascii Table

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
 
+        Date now = new Date();
+        System.out.println(now);
+
+        for (int i = 0; i < 128; i++) {
+            System.out.printf("char: %c | Decimal: %d | Binary: %s \n", i, i, Integer.toBinaryString(i));
+        }
     }
 }
