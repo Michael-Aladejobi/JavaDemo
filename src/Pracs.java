ca07418d3c0bf37b1f7f4e291295251fea08ee0e
Michael-Aladejobi <mikkystechs@gmail.com>
2024-11-01 10:43:51 +0100

    ...

--- src/Pracs.java
+++ src/Pracs.java
@@ -1,7 +1,26 @@
 public class Pracs {
     public static void main(String[] args) {
-        for (int i = 0; i < 128; i++) {
-            System.out.printf("Char: %c | Decimal:%d | Binary: %s \n", i, i, Integer.toBinaryString(i));
+        // for (int i = 0; i < 128; i++) {
+        //     System.out.printf("Char: %c | Decimal:%d | Binary: %s \n", i, i, Integer.toBinaryString(i));
+        // }
+
+        int temperature = 32;
+
+        if (temperature > 30){
+            System.out.println("It's a hot day \nDrink plenty of water");
+        } else if (temperature >= 20 && temperature <= 30){
+            System.out.println("Beutiful Day");
+        } else {
+            System.out.println("Cold day!");
         }
+
+        // boolean isWarm = temperature > 20 && temperature < 32;
+        // System.out.print(isWarm);
+
+        // boolean hasHighIncome = true;
+        // boolean hasGoodCredit = true;
+        // boolean isEligible = hasHighIncome || hasGoodCredit;
+
+
     }
 }
