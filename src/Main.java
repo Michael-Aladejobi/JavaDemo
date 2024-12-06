874942801b71e3c987ef70532fe5a816c7a05090
Michael-Aladejobi <mikkystechs@gmail.com>
2024-12-06 17:12:47 +0100

    ...

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,127 @@
+class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+            i += 2;
+        }
+    }
+}
+
+// class Main {
+// public static void main(String[] args) {
+// String[] names = { "Mike", "Ann", "Lizzy", "Sammy" };
+
+// for (String name : names) {
+// int count = 1;
+// System.out.println(name + 'z');
+// }
+
+// }
+// }
+
+// class Main {
+// public static void main(String[] args) {
+// int sum = 0;
+
+// for (int i = 0; i <= 5; i++) {
+// sum += i;
+
+// }
+
+// System.out.println(sum);
+// }
+// }
+
+// class Main {
+// public static void main(String[] args) {
+// int score = 1;
+
+// switch (score) {
+// case 1:
+// System.out.println("1");
+// break;
+// case 2:
+// System.out.println("2");
+// break;
+// default:
+// System.out.println("NAN");
+// }
+// }
+// }
+
+// public class Main {
+// public static void main(String[] args) {
+// int arr[] = { 1, 2, 3, 4, 5 };
+
+// for (int i = 0; i < arr.length; i++) {
+// System.out.println(arr[i]);
+// }
+
+// }
+// }
+
+// public class Main {
+// public static void main(String[] args) {
+// int x = 30;
+// int y = 40;
+// int temp;
+
+// temp = x;
+// x = y;
+// y = temp;
+
+// System.out.println(x);
+// System.out.println(y);
+
+// }
+// }
+
 // import java.util.Scanner;
 
 // public class Main {
-//     public static void main(String[] args) {
-//        int age;
-//        age = 0;
-//        if( age >= 18){
-//         System.out.println("Approved");
-//        }
-//        else if(age == 0)
-//        else {
-//         System.out.println("Not Approved");
-//        }
-//     }
+// public static void main(String[] args) {
+// int age;
+// age = 0;
+// if( age >= 18){
+// System.out.println("Approved");
+// }
+// else if(age == 0)
+// else {
+// System.out.println("Not Approved");
+// }
+// }
 // }
 
 // //Michael Aladejobi
 
-class Main {
+// class Main {
 
-    public static void main(String args[]) {
+// public static void main(String args[]) {
 
-        byte x;
+// byte x;
 
-        int a = 270;
+// int a = 270;
 
-        double b = 128.128;
+// double b = 128.128;
 
-        System.out.println("int converted to byte");
+// System.out.println("int converted to byte");
 
-        x = (byte) a;
+// x = (byte) a;
 
-        System.out.println("a and x " + a + " " + x);
+// System.out.println("a and x " + a + " " + x);
 
-        System.out.println("double converted to int");
+// System.out.println("double converted to int");
 
-        a = (int) b;
+// a = (int) b;
 
-        System.out.println("b and a " + b + " " + a);
+// System.out.println("b and a " + b + " " + a);
 
-        System.out.println("\ndouble converted to byte");
+// System.out.println("\ndouble converted to byte");
 
-        x = (byte) b;
+// x = (byte) b;
 
-        System.out.println("b and x " + b + " " + x);
+// System.out.println("b and x " + b + " " + x);
 
-    }
+// }
 
-}
+// }
