61fae2d01099dcfd1a2effb6e20b772985efa984
Michael-Aladejobi <mikkystechs@gmail.com>
2024-12-06 17:13:07 +0100

    ...

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
-class Main {
-    public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++) {
-            System.out.println(i);
-            i += 2;
-        }
-    }
-}
+
+// class Main {
+// public static void main(String[] args) {
+// for (int i = 0; i <= 10; i++) {
+// System.out.println(i);
+// i += 2;
+// }
+// }
+// }
 
 // class Main {
 // public static void main(String[] args) {
