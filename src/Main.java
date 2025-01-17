014fac47a72d1f1800711b80daa188f4d55abdd8
Michael-Aladejobi <mikkystechs@gmail.com>
2025-01-17 03:52:04 +0100

    ...

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,13 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = { 1, 2, 3, 4, 5 };
+        int[][] numbers = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
 
         System.out.println("length: " + numbers.length);
-        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.deepToString(numbers));
     }
 }
