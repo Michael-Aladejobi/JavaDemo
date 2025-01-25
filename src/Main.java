e0ccd8bf69a2fe18e2673561b3d3aaff05ea1a8e
Michael-Aladejobi <mikkystechs@gmail.com>
2025-01-25 07:52:54 +0100

    ...

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String input1 = input.nextLine();
+
+        System.out.print("Enter second string: ");
+        String input2 = input.nextLine();
+
+        String totalString = input1 + input2;
+        System.out.println(totalString);
 
     }
 }
