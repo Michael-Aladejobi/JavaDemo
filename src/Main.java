86cabac470d5ce0292b24dae9aad56db2ccd02c2
Michael-Aladejobi <mikkystechs@gmail.com>
2025-01-25 09:09:44 +0100

    ....

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] months = { "January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December" };
+        int[] days = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        for (int i = 0; i <= months.length; i++) {
+
+        }
+    }
+}
