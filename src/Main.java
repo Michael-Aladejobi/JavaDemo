ee20cc03b2c747a3ef7d20ad0473404260a16806
Michael-Aladejobi <mikkystechs@gmail.com>
2025-01-25 10:49:42 +0100

    hd1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("How many number do you want to enter: ");
 
     }
 }
