bc0a6eff8597a87b25d767b9c1b193898869437d
Michael-Aladejobi <mikkystechs@gmail.com>
2025-01-25 11:07:43 +0100

    ....

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("How many integer element do you want to enter? : ");
+        int size = scanner.nextInt();
+
+        int numbers[] = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter number " + i + ": ");
+            numbers[i] = scanner.nextInt();
+        }
+
+        System.out.println("Enter number to check: ");
+        int checkNum = scanner.nextInt();
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == checkNum) {
+                System.out.println(checkNum + " is in array.");
+                break;
+            } else {
+                System.out.println(checkNum + " is NOT in array!!!");
+            }
+            break;
+        }
     }
 }
