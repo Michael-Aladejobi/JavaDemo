1a8ed02006d2b338d3e1a8f4e6785448e782d61c
Michael-Aladejobi <mikkystechs@gmail.com>
2025-01-25 10:02:05 +0100

    ,,,,

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,15 @@ public class Main {
         int[] days = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
         for (int i = 0; i <= months.length; i++) {
-            System.out.println(months[i] + " : " + days[i]);
+            System.out.println(months[i] + ": " + days[i] + " days.");
+        }
+
+        System.out.println(".........................................");
+        System.out.println("\nMonths with 30dys.");
+        for (int j = 0; j <= months.length; j++) {
+            if (days[j] == 30) {
+                System.out.println(months[j]);
+            }
         }
     }
 }
