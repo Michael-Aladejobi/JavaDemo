b723889c17111bfbace038052f272d798dfcfdef
Michael-Aladejobi <mikkystechs@gmail.com>
2025-01-26 00:51:57 +0100

    ..

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("How many integer element do you want to enter? : ");
-        int size = scanner.nextInt();
+        System.out.print("Enter word:");
+        String word = scanner.nextLine();
 
-        int numbers[] = new int[size];
+        int len = word.length();
 
-        for (int i = 0; i < size; i++) {
-            System.out.print("Enter number " + i + ": ");
-            numbers[i] = scanner.nextInt();
+        int middle = len / 2;
+
+        if (len % 2 == 0) {
+            System.out.println("Middle Char: " + word.charAt(middle - 1) + word.charAt(middle));
+        } else {
+
         }
 
-        System.out.println("Enter number to check: ");
-        int checkNum = scanner.nextInt();
-
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] == checkNum) {
-                System.out.println(checkNum + " is in array.");
-                break;
-            } else {
-                System.out.println(checkNum + " is NOT in array!!!");
-            }
-            break;
-        }
     }
 }
