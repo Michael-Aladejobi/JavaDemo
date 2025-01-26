4db7096ca5889dd5ae6417639d302d5b5e675f23
Michael-Aladejobi <mikkystechs@gmail.com>
2025-01-26 07:46:50 +0100

    ...

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,12 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("How many numbers do you want to store? : ");
-        int size = scanner.nextInt();
-
-        int numbers[] = new int[size];
-
-        System.out.println("Please enter numbers: ");
-        for (int i = 0; i < size; i++) {
-            System.out.print("Enter number " + (i + 1) + ": ");
-            numbers[i] = scanner.nextInt();
-
-        }
-
-        Arrays.sort(numbers);
-
-        System.out.print("Sorted Array: ");
-        for (int number : numbers) {
-            System.out.println(number);
-        }
-
+        System.out.println("Please enter a sentecnce: ");
+        String sentence = scanner.nextLine();
         scanner.close();
+
     }
 }
